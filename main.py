73e55e1c4a5c6c322fa3cb223a082978b8566f39
Lehtty <leticia-olive@hotmail.com>
2023-10-06 20:13:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
+
+# fila_teste = filanormal()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# fila_teste.atualizafila()
+# print(fila_teste.chamacliente(5))
+
+# fila_teste_2 = FilaNormal()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# fila_teste_2.atualiza_fila()
+# print(fila_teste_2.chama_cliente(10))
+# print(fila_teste_2.chama_cliente(1))
+
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica(EstatisticaResumida('20/03/2025', 120)))
